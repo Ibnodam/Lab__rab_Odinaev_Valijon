5408ceaa84d222cc073e5da5fa6c5f84ab6ecc81
dns <dns@LAPTOP-EHRCLL3N>
2023-09-25 21:46:05 +0200

    Добавьте файлы проекта.

---
+++ Lab__rab_1_Odinaev/Program.cs
@@ -0,0 +1,38 @@
+﻿//Console.Write("Введите значение x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Введите значение y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.Write("Введите значение a:");
+//double a = double.Parse(Console.ReadLine());
+//double numerator = Math.Cos((Math.Pow(x, 3)) + 6) - Math.Sin(y - a);
+//double denominator = Math.Log(Math.Pow(x, 4)) - 2 * Math.Pow(Math.Sin(x), 5);
+//double D = numerator / denominator;
+//Console.WriteLine($"D = {D}");
+
+
+
+
+//Console.Write("Введите координату a1:");
+//double a1 = double.Parse(Console.ReadLine());
+//Console.Write("Введите координату a2:");
+//double a2 = double.Parse(Console.ReadLine());
+//Console.Write("Введите координату b1:");
+//double b1 = double.Parse(Console.ReadLine());
+//Console.Write("Введите координату b2:");
+//double b2 = double.Parse(Console.ReadLine());
+//double numerator = (a1*b1)+(a2*b2);
+//double denominator = Math.Sqrt((Math.Pow(a1,2)+Math.Pow(a2,2)))*Math.Sqrt(Math.Pow(b1,2)+Math.Pow(b2,2));
+//Double Cos_alpha = numerator/denominator;
+//Console.WriteLine($"Cos_alpha = {Cos_alpha}");
+
+
+
+
+
+
+
+
+
+
+
+
